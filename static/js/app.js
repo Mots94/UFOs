a46acdd3183cc5f2ef6e9dc5fc70a74cba9fa22a
Mots94 <rmotykow@gmail.com>
2022-05-18 16:09:35 -0500

    building filter table function

--- static/js/app.js
+++ static/js/app.js
@@ -10,11 +10,21 @@ let tbody = d3.select("tbody");
 
 function buildTable(data) {
 
+    // Clear out existing data
+    
     tbody.html("");
+
+    // Loop through each object in data
+    // and append a row and cells for each value
 
     data.forEach((dataRow) => {
 
+        // Append row to the table body
+
         let row = tbody.append("tr");
+
+        // Loop through each field in the dataRow and
+        // add each value as a table cell
 
         Object.values(dataRow).forEach((val) => {
 
@@ -25,4 +35,29 @@ function buildTable(data) {
             }
         );
     });
+}
+
+function handleClick() {
+
+    // Grab the datetime value from the filter
+
+    let date = d3.select("#datetime").property("value");
+
+    let filteredData = tableData;
+
+    // If a date is set, filter data on date
+
+    if (date) {
+
+        // Apply filter to table if rows where
+        // 'datetime' value matches filter value
+
+        filteredData = filteredData.filter(row => row.datetime === date);
+    
+    };
+
+    buildTable(filteredData);
+
+    // else return original set of data
+
 }
