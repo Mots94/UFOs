6e3a7341f93079abafc9cb4f4befaede6fb1d319
Mots94 <rmotykow@gmail.com>
2022-05-18 15:54:37 -0500

    javascript function data table

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,27 @@
 
 const tableData = data;
 
-// Rference the HTML table using d3
+// Reference the HTML table using d3
 
-var tbody = d3.select("tbody");
+let tbody = d3.select("tbody");
+
+// Start a function to iterate over data and add it to a table
+
+function buildTable(data) {
+
+    tbody.html("");
+
+    data.forEach((dataRow) => {
+
+        let row = tbody.append("tr");
+
+        Object.values(dataRow).forEach((val) => {
+
+            let cell = row.append("td");
+
+            cell.text(val);
+
+            }
+        );
+    });
+}
